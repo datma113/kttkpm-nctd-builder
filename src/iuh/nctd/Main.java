2e41df3ac71f14f53042acb1b7d5245e40e40449
datma <datma113112111@gmail.com>
2021-11-17 09:57:56 +0700

    Initial commit

---
+++ src/iuh/nctd/Main.java
@@ -0,0 +1,17 @@
+package iuh.nctd;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Computer computer = new Computer.ComputerBuilder("hdd", "ram")
+                .build();
+
+        Computer computer1 = new Computer.ComputerBuilder("hdd", "ram")
+                .setBluetoothEnabled(true)
+                .setGraphicsCardEnabled(true)
+                .build();
+
+        
+    }
+}
