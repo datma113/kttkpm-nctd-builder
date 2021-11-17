a994dfe0d4059b25d491a5565944f9b7c8f8e802
datma <datma113112111@gmail.com>
2021-11-17 09:58:29 +0700

    done computer builder

--- src/iuh/nctd/Main.java
+++ src/iuh/nctd/Main.java
@@ -12,6 +12,7 @@ public class Main {
                 .setGraphicsCardEnabled(true)
                 .build();
 
-        
+        System.out.println(computer);
+        System.out.println(computer1);
     }
 }
